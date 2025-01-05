bc32e7bb3217e37fa1f3920ab7c6016cb57bc451
SadikRa <sadikrahman494@gmail.com>
2025-01-05 16:51:09 +0600

    form added done

--- src/components/module/addTaskModal.tsx
+++ src/components/module/addTaskModal.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -18,11 +19,21 @@ import {
 } from "../ui/form";
 import { useForm } from "react-hook-form";
 import { Input } from "../ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export function AddTaskModal() {
   const form = useForm();
 
-  const onSubmit = () => {};
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <Dialog>
@@ -30,29 +41,88 @@ export function AddTaskModal() {
         <Button variant="outline">Add Task</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
+        <DialogDescription className="sr-only">
+          fill up this form to this task
+        </DialogDescription>
         <DialogHeader>
           <DialogTitle>Add Task</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
+          <form className="space-y-10" onSubmit={form.handleSubmit(onSubmit)}>
             <FormField
               control={form.control}
               name="title"
-              render={() => (
+              render={({ field }) => (
                 <FormItem>
-                  <FormLabel />
+                  <FormLabel> Title</FormLabel>
                   <FormControl>
-                    <Input></Input>
+                    <Input {...field} value={field.value || ""}></Input>
                   </FormControl>
                   <FormDescription />
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel> Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""}></Textarea>
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="deuDate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel> Due Date</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""}></Textarea>
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="priority"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Priority</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified email to display" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="low">low</SelectItem>
+                      <SelectItem value="medium">medium</SelectItem>
+                      <SelectItem value="high">high</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
           </form>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
         </Form>
       </DialogContent>
     </Dialog>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface initialState {
   tasks: ITask[];
-  filter: "all" | "high" | "medium" | "low"
+  filter: "all" | "high" | "medium" | "low";
 }
 
 const initialState: initialState = {
@@ -18,16 +18,15 @@ const initialState: initialState = {
       priority: "low",
     },
     {
-      id: "2324",
+      id: "234",
       title: "init",
       description: "create home page and routing",
       dueDate: "2025",
       isCompleted: false,
       priority: "high",
     },
-    
   ],
-  filter: "all"
+  filter: "all",
 };
 
 const taskSlice = createSlice({
@@ -38,6 +37,5 @@ const taskSlice = createSlice({
 
 export const selectTasks = (state: RootState) => state.todo.tasks;
 export const selectFilter = (state: RootState) => state.todo.filter;
-
 
 export default taskSlice.reducer;
